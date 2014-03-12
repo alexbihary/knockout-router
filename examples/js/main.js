b2385552283fac8e25cdcfee14fda7bf1f06d892
alexbihary <alex.bihary@gmail.com>
2014-03-12 14:56:18 +0000

    update baseurl

--- examples/js/main.js
+++ examples/js/main.js
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: 'examples',
+  baseUrl: '/knockout-router/examples',
   paths: {
     'knockout': 'lib/knockout-3.0.0',
     'knockout-amd-helpers': 'lib/knockout-amd-helpers-0.6.1',
