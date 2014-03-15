f0eae5fc7b8134c18323cb3d0a45507dd90e691f
Alexander Bihary <alex.bihary@gmail.com>
2014-03-15 19:44:44 -0400

    add root path for example

--- examples/js/app.js
+++ examples/js/app.js
@@ -1,28 +1,28 @@
 define(['knockout', 'knockout-router'], function(ko) {
-  
+
   function run() {
-    var vm = { 
+    var vm = {
       router: ko.router.vm,
-      moduleToShow: ko.observable()      
+      moduleToShow: ko.observable()
     };
-    
+
     // Can attach a global subscription to be notified for all route changes.
     function notify() {
       console.log(arguments, new Date());
     }
-    
+
     // Can attach a 'route not found' handler, which will be passed a url fragment and query string.
     function notFoundHandler(fragment, query) {
       console.log(fragment, query);
     }
-    
+
     // Configure routing options before defining routes
-    ko.router.configure({ hashPrefix: '#/', debug: true, notify: notify, pushState: true });
-    
+    ko.router.configure({ hashPrefix: '#/', debug: true, notify: notify, pushState: true, root: '/knockout-router/' });
+
     // Configure module loader
     ko.bindingHandlers.module.baseDir = 'js';
-    
-    // Define the routes before ko.applyBindings() 
+
+    // Define the routes before ko.applyBindings()
     ko.router.map([
       { route: '', name: 'home', template: 'home', title: 'Welcome', nav: true },
       { route: 'who-we-are', name: 'about', title: 'About Us', nav: true },
@@ -31,18 +31,18 @@ define(['knockout', 'knockout-router'], function(ko) {
       //{ route: 'blog(/:slug)', name: 'blog', title: 'Crazy Blog' },
       //{ route: '*notfound', name: 'notfound', callback: notFoundHandler }
     ]).mapNotFound({ callback: notFoundHandler }); // can specify a module/template/callback/title
-    
+
     // Bind the view model
     ko.applyBindings(vm);
-    
+
     // Should call ko.router.init() after ko.applyBindings() when using
-    // the route bindingHandler. otherwise could safely call ko.router.init() 
+    // the route bindingHandler. otherwise could safely call ko.router.init()
     // once the routes have been defined.
     ko.router.init(); //calls ko.history.start() behind the scenes
   }
-  
+
   return {
     run: run
   };
-  
+
 });
